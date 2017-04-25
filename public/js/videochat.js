150f02c0be1cbcbe74861bde5b65e558ee579a73
Tristan Watanabe <tristan.watanabe@gmail.com>
2017-04-25 13:24:57 -0400

    VideoChat page added

---
+++ public/js/videochat.js
@@ -0,0 +1,25 @@
+
+
+var session = OT.initSession(sessionId);
+var options = {
+	insertMode: 'append',
+	width: 480,
+	height: 320
+};
+
+var publisher = OT.initPublisher("publisher", options);
+
+session.connect(apiKey, token, function(err, info) {
+  if(err) {
+    alert(err.message || err);
+  }
+  session.publish(publisher);
+});
+
+
+
+session.on('streamCreated', function(event) {
+  session.subscribe(event.stream, "subscribers", options);
+});
+
+
--- server.js
+++ server.js
@@ -16,6 +16,9 @@ var session      = require('express-session');
 
 var configDB = require('./config/database.js');
 
+var request = require('request');
+var OpenTok = require('opentok');
+
 // ejs setup display images / stylesheets =============================================================
 
 app.use(express.static('public'));
@@ -42,6 +45,35 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// required for Opentok==========================================================
+
+// Opentok API Key and Secret
+var apiKey = "45827022",
+    apiSecret = "a3e34c57968fb9b6e83119489e05c11145e3ce61";
+if (!apiKey || !apiSecret) {
+  console.log('You must specify API_KEY and API_SECRET environment variables');
+  process.exit(1);
+}
+
+// Initialize OpenTok
+var opentok = new OpenTok(apiKey, apiSecret);
+//Variable keeps track of the session ID for the URL
+var sessID;
+
+// Create a session and store it in the express app
+opentok.createSession({ mediaMode: 'routed', archiveMode: 'always' },function(err, session) {
+  if (err) throw err;
+  app.set('sessionId', session.sessionId);
+
+  //initializes sessID to be appended to URL
+  sessID = session.sessionId;
+    
+});
+
+
+
+
 
 // launch ======================================================================
 app.listen(port);
